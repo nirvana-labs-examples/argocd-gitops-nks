dfdff41573bcb49a09b4c5278c2c8cd29e0847cc
James Wu <james@nirvanalabs.io>
2026-04-21 11:09:13 -0400

    fix(bootstrap-tf): pin helm/kubernetes/kubectl to v2.x for compat with plan syntax

--- examples/bootstrap-tf/versions.tf
+++ examples/bootstrap-tf/versions.tf
@@ -7,15 +7,15 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.13"
+      version = "~> 2.13"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.30"
+      version = "~> 2.30"
     }
     kubectl = {
       source  = "alekc/kubectl"
-      version = ">= 2.0"
+      version = "~> 2.0"
     }
     local = {
       source  = "hashicorp/local"
